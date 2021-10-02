087e6ca3cffb57ca0cad9999f39e79ef4fd9c714
filippa00 <filippa00@users.noreply.github.com>
2021-10-02 16:03:13 +0200

    week4Loginscreen

--- src/main/java/nl/inholland/javafx/App.java
+++ src/main/java/nl/inholland/javafx/App.java
@@ -1,110 +1,113 @@
 package nl.inholland.javafx;
-
 import javafx.application.Application;
+import javafx.beans.property.StringProperty;
 import javafx.beans.value.ChangeListener;
 import javafx.beans.value.ObservableValue;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.geometry.Insets;
-import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.GridPane;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
-import java.awt.event.MouseEvent;
-import java.awt.event.MouseListener;
-import java.util.regex.Pattern;
-import java.util.regex.Matcher;
 
-public class App extends Application implements MouseListener {
+public class App<loginIsValidPassword> extends Application{
 
+    Label userLabel,passwordLabel,errorMessage;
+    TextField userInput;
     PasswordField passwordField;
     Button loginButton;
-    String passwordText;
-
     @Override
-    public void start(Stage window) throws Exception {
+    public void start(Stage window) throws Exception{
 
         window.setHeight(200);
-        window.setWidth(400);
-        window.setTitle("Inholland JavaFX Starter Project");
+        window.setWidth(320);
+        window.setTitle("Login Window");
 
+        userLabel = new Label("Username:");
+        passwordLabel = new Label("Password:");
+        userInput = new TextField();
+        userInput.setPromptText("Username");
+        passwordField = new PasswordField();
+        passwordField.setPromptText("password");
+        loginButton = new Button("log in");
+        errorMessage = new Label();
 
+        VBox container = new VBox();
         GridPane gridPane = new GridPane();
-        gridPane.setPadding(new Insets(10, 10, 10, 10));
+        gridPane.setPadding(new Insets(10));
         gridPane.setVgap(10); // Vertical spacing between grid items
         gridPane.setHgap(8); // Horizontal spacing between grid items
-        loginButton = new Button();
-       loginButton.setText("Log in");
-        gridPane.add(loginButton, 2, 3);
+        errorMessage.setPadding((new Insets(10)));
+
+        GridPane.setConstraints(userLabel,0,0);
+        GridPane.setConstraints(userInput,1,0);
+        GridPane.setConstraints(passwordLabel,0,1);
+        GridPane.setConstraints(passwordField,1,1);
+        GridPane.setConstraints(loginButton,1,2);
 
 
-        Label usernameLabel = new Label("Username:");
-        Label passwordLabel = new Label("Password:");
-        gridPane.add(usernameLabel, 1, 1);
-        gridPane.add(passwordLabel, 1, 2);
 
-        TextField usernameInput = new TextField();
-        passwordField = new PasswordField();
-        passwordText = passwordField.getText();
-        gridPane.add(usernameInput, 2, 1);
-        gridPane.add(passwordField, 2, 2);
 
-        loginButton.setDisable(true);
-        Scene scene = new Scene(gridPane);
+        StringProperty passwordFieldProperty = passwordField.textProperty();
+        //errorMessage.textProperty().bind(passwordFieldProperty);
+        passwordFieldProperty.addListener(new ChangeListener<String>() {
+            @Override
+            public void changed(ObservableValue<? extends String> observableValue,
+                                String oldValue, String newValue) {
+                   loginButton.setVisible(loginIsValidPassword(newValue));
+
+            }
+
+        });
+        loginButton.setVisible(false);
+        loginButton.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent actionEvent) {
+                if (loginIsValidPassword(passwordField.getText()) && passwordField.getText().equalsIgnoreCase("p@ssword123"))
+                {
+                    MainWindow mainWindow = new MainWindow(userInput.getText());
+                    window.close();
+                }
+                else
+                {
+                    errorMessage.setText("wrong password");
+                }
+            }
+        });
+
+        gridPane.getChildren().addAll(userLabel,userInput,passwordLabel,passwordField,loginButton,errorMessage);
+        container.getChildren().addAll(gridPane,errorMessage);
+
+        Scene scene = new Scene(container);
         window.setScene(scene);
         window.show();
-
-        passwordField.getOnMouseClicked();
     }
 
-    static boolean correctPassowrdLength(PasswordField passwordField) {
-        return passwordField.getLength() < 8;
-    }
+     private Boolean loginIsValidPassword(String password){
+        boolean hasLetters = false;
+        boolean hasDigits = false;
+        boolean hasSpecialCharacters = false;
 
-    static boolean passowrdContainsLetter(String passwordText) {
-        Pattern letter = Pattern.compile("(?=.*[A-Za-z])");
-        Matcher matcher = letter.matcher(passwordText);
-        return matcher.matches();
-    }
+        for(char c : password.toCharArray())
+        {
+            if(Character.isDigit(c))
+            {
+                hasDigits = true;
+            }
+            else if(Character.isLetter(c))
+            {
+                 hasLetters = true;
+            }
+            else {
+                hasSpecialCharacters = true;
+            }
 
-    static boolean passowrdContainsSpecialCharacter(String passwordText) {
-        Pattern specialChar = Pattern.compile("(?=.*[@#$%^&-+=()])");
-        Matcher matcher = specialChar.matcher(passwordText);
-        return matcher.matches();
-    }
-
-    static boolean passowrdContainsNumber(String passwordText) {
-        Pattern number = Pattern.compile("(?=.*[0-9]) ");
-        Matcher matcher = number.matcher(passwordText);
-        return matcher.matches();
-    }
-
-    @Override
-    public void mouseClicked(MouseEvent e) {
-        if (correctPassowrdLength(passwordField) && passowrdContainsLetter(passwordText) && passowrdContainsSpecialCharacter(passwordText) && passowrdContainsNumber(passwordText)) {
-            loginButton.setDisable(false);
         }
-    }
-
-    @Override
-    public void mousePressed(MouseEvent e) {
-
-    }
-
-    @Override
-    public void mouseReleased(MouseEvent e) {
-
-    }
-
-    @Override
-    public void mouseEntered(MouseEvent e) {
-
-    }
-
-    @Override
-    public void mouseExited(MouseEvent e) {
-
+        return password.length()>=8 &&(hasSpecialCharacters && hasLetters && hasDigits);
     }
 }
